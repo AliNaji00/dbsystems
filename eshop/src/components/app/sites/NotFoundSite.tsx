80e3fc73ac3f3f9c71c8889da7e7d657009d5229
Florian <florian@chengine.com>
2023-05-13 18:48:15 +0800

    Added NotFoundPage, Text Styles

--- eshop/src/components/app/sites/NotFoundSite.tsx
+++ eshop/src/components/app/sites/NotFoundSite.tsx
@@ -2,9 +2,30 @@ import * as React from "react";
 import { CenteredContent } from "../../ui/CenteredContent";
 import { RouteNames } from "../router/RouteNames";
 import { Link } from "react-router-dom";
+import { undraw_page_not_found_re_e9o6 } from "../../util/Images";
+import { Button } from "@mui/material";
 
 export const NotFoundSite = () => (
-  <CenteredContent>
-    <Link to={RouteNames.LOG_IN}>Test</Link>
-  </CenteredContent>
+  <div
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      height: "calc(100vh - 64px)",
+    }}
+  >
+    <h1 style={{ textAlign: "center" }}>Oops, page not found :(</h1>
+    <h3 style={{ textAlign: "center", marginBottom: 50 }}>
+      Don't worry, we've got your back!
+    </h3>
+    <img
+      src={undraw_page_not_found_re_e9o6}
+      alt="Page not found"
+      style={{ maxWidth: 500, marginBottom: 100 }}
+    />
+    <Link to={RouteNames.LOG_IN}>
+      <Button variant="contained">Return Home</Button>
+    </Link>
+  </div>
 );
---
+++ eshop/src/components/util/Images.tsx
@@ -0,0 +1,3 @@
+import undraw_page_not_found_re_e9o6 from "../../assets/images/undraw_page_not_found_re_e9o6.svg";
+
+export { undraw_page_not_found_re_e9o6 };
