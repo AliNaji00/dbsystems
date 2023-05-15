70bc44b5dd8efc484aede806939a65c01606032f
Florian <florian@chengine.com>
2023-05-15 17:38:23 +0800

    updated headers to work with both frontends

--- backend/php/headers.php
+++ backend/php/headers.php
@@ -1,7 +1,21 @@
 <?php
 header('Content-Type: application/json; charset=UTF-8');
-header("Access-Control-Allow-Origin: http://localhost:3000");
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Cookie');
 header('Access-Control-Allow-Credentials: true');
+
+// Set an array of allowed origins
+$allowedOrigins = array(
+  'http://localhost:3000',
+  'http://localhost:8080'
+);
+
+// Check if the request's origin header exists and it's in the allowed origins array
+if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
+  header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+} else {
+  // Deny access if the origin is not allowed
+  header('HTTP/1.1 403 Forbidden');
+  exit;
+}
 ?>
