cabaec972fbaafdf7235d73fdda32c0babfe7385
Florian <florian@chengine.com>
2023-06-04 17:43:54 +0800

    added CustomerProductSite

--- eshop/src/components/customer/sites/CustomerProductSite.tsx
+++ eshop/src/components/customer/sites/CustomerProductSite.tsx
@@ -8,6 +8,11 @@ import { useParams } from "react-router-dom";
 import { useGeneralStore } from "../../../stores/GeneralStore";
 import { useProduct } from "../../../stores/useProduct";
 import { getSingleProductResponseMockData } from "../../network/APITypes";
+import { getImagePath } from "../../util/Helpers";
+import { customColors } from "../../util/Theme";
+import { CustomNumberField } from "../../ui/ProductCartNumberField";
+import { API } from "../../network/API";
+import { Avatar } from "@mui/material";
 
 type ProductIdParam = {
   productId: string;
@@ -35,8 +40,105 @@ export const CustomerProductSite = () => {
       <CustomerBackground style={{ minHeight: 200 }}>
         <CenteredContent>
           <div
-            style={{ backgroundColor: "black", height: 200, width: 500 }}
-          ></div>
+            style={{
+              width: "100%",
+              display: "flex",
+              paddingBottom: 32,
+              borderBottom: `1px solid ${customColors.primaryColor}`,
+            }}
+          >
+            <div
+              style={{
+                flex: 11, // This will take up 1/4 of the space
+                backgroundColor: customColors.primaryColor,
+                alignSelf: "flex-start",
+              }}
+            >
+              <img
+                src={getImagePath(product.ImageURL)}
+                alt={product.name}
+                style={{
+                  width: "100%", // Adjusts the width to fill the parent container
+                  height: "auto",
+                }}
+              />
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                flex: 20,
+                padding: 20,
+                marginLeft: 48,
+              }}
+            >
+              <h2>{product.name}</h2>
+              <p style={{ fontSize: 16, marginBottom: 32 }}>
+                {product.description}
+              </p>
+              {!!product.original_price && (
+                <div style={{ display: "flex" }}>
+                  <h4 style={{ flex: 1 }}>
+                    <del>Price: $ {product.original_price}</del>
+                  </h4>
+                  <h4 style={{ color: customColors.tomato, flex: 4 }}>
+                    {product.coupon_code}: {product.coupon_description}
+                  </h4>
+                </div>
+              )}
+              <h4
+                style={{
+                  marginTop: 8,
+                }}
+              >
+                Price: $ {product.price}
+              </h4>
+              <div style={{ maxWidth: 100, alignSelf: "end" }}>
+                <CustomNumberField
+                  initialAmount={product.AmountInBasket}
+                  maxAmount={product.stock_quantity}
+                  changeValue={async (newNumber: number) => {
+                    try {
+                      generalStore.isLoading = true;
+
+                      await API.putBasket(
+                        generalStore.userId,
+                        product.product_id,
+                        newNumber
+                      );
+                    } catch (err) {
+                      console.log(err);
+                    } finally {
+                      generalStore.isLoading = false;
+                      generalStore.productsChangeFlag =
+                        !generalStore.productsChangeFlag;
+
+                      generalStore.productChangeFlag =
+                        !generalStore.productChangeFlag;
+                    }
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+          <div style={{ display: "flex", padding: 16, gap: 16 }}>
+            <Avatar
+              src={getImagePath(product.seller_image_url)}
+              sx={{ bgcolor: customColors.primaryColor }}
+            />
+            <div style={{ maxWidth: 300, paddingTop: 3 }}>
+              <h3>{product.store_name}</h3>
+              <p style={{ fontSize: 16, margin: "8px 0" }}>
+                {product.store_email}
+              </p>
+              <p style={{ fontSize: 16, margin: "8px 0" }}>
+                {product.store_phone}
+              </p>
+              <p style={{ fontSize: 16, margin: "8px 0" }}>
+                {product.store_address}
+              </p>
+            </div>
+          </div>
         </CenteredContent>
       </CustomerBackground>
     </>
--- eshop/src/components/util/Helpers.ts
+++ eshop/src/components/util/Helpers.ts
@@ -1,6 +1,9 @@
 import { imgageHost } from "../../stores/GeneralStore";
 
-export const getImagePath = (path: string | null) => {
+export const getImagePath = (path: string | null | undefined) => {
   // remove /img/ from path
-  return `${imgageHost}${path}`;
+  if (path) {
+    return `${imgageHost}${path}`;
+  }
+  return "";
 };
