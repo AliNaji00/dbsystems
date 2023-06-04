380c2a203af60c0fbcbde6bd299b2f599715e4e9
Florian <florian@chengine.com>
2023-06-04 15:02:22 +0800

    fixed image paths

--- eshop/src/components/util/Helpers.ts
+++ eshop/src/components/util/Helpers.ts
@@ -4,11 +4,5 @@ import { imgageHost } from "../../stores/GeneralStore";
 
 export const getImagePath = (path: string | null) => {
   // remove /img/ from path
-  if (path !== null) {
-    path = path.replace("./img/", "");
-    path = path.replace("/img/", "");
-    return `${imgageHost}/${path}`;
-  }
-
-  return "";
+  return `${imgageHost}${path}`;
 };
--- eshop/src/stores/GeneralStore.tsx
+++ eshop/src/stores/GeneralStore.tsx
@@ -5,7 +5,7 @@ import { makePersistable } from "mobx-persist-store";
 import { UserRole } from "../components/network/APITypes";
 
 export const SEARCH_DEBOUNCE_MS = 500;
-export const imgageHost = "http://localhost:8080/api/img";
+export const imgageHost = "http://localhost:8080";
 
 export class GeneralStore {
   keyword = "";
