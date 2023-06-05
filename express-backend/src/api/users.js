309138db98ed9efe6d0ae5d587e9f9b8151ae60a
Elias <franzflach303@gmail.com>
2023-06-05 14:31:37 +0800

    Add user update endpoint

--- express-backend/src/api/users.js
+++ express-backend/src/api/users.js
@@ -61,9 +61,54 @@ export default ({ pool }) => {
       } catch (err) {
         conn.rollback();
         console.log(err);
-        res.status(400).json({ msg: "error creating user" });
+        res
+          .status(400)
+          .json({ msg: "error creating user: " + err.text.split("\n")[0] });
       }
       conn.close();
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  route.put("/:user_id", async (req, res, next) => {
+    const user_id = req.params.user_id;
+    console.log(user_id);
+    // if user_type is not "seller", we can still update the "users" entry of a seller
+    const user_type = req.body.user_type;
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const address = req.body.address;
+    try {
+      const conn = await pool.getConnection();
+      await conn.beginTransaction();
+      try {
+        await conn.query(
+          "UPDATE users SET name = ?, email = ?, password = ?, address = ? WHERE user_id = ?",
+          [name, email, password, address, user_id]
+        );
+        if (user_type === "seller") {
+          const store_name = req.body.store_name;
+          const store_address = req.body.store_address;
+          const phone_no = req.body.phone_no;
+          const store_email = req.body.store_email;
+          await conn.query(
+            "UPDATE seller SET store_name = ?, store_address = ?, phone_no = ?, store_email = ? WHERE user_id = ?",
+            [store_name, store_address, phone_no, store_email, user_id]
+          );
+        }
+        await conn.commit();
+        res.status(200).send();
+      } catch (err) {
+        conn.rollback();
+        console.log(err);
+        res
+          .status(400)
+          .json({ msg: "error updating user: " + err.text.split("\n")[0] });
+      } finally {
+        conn.close();
+      }
     } catch (err) {
       next(err);
     }
